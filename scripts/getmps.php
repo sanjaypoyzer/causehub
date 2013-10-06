2f1f135db2b2bbed0f381b0355e68d5e06f6ea8b
John Bywater <john.bywater@appropriatesoftware.net>
2013-10-06 17:33:34 +0100

    More getmps changes...

--- scripts/getmps.php
+++ scripts/getmps.php
@@ -1,9 +1,7 @@
 <?php
 	session_start();
-	$dbpath = ($_SERVER['DOCUMENT_ROOT'].'/mps.db');
-    echo $dbpath;
-    echo "\nDatabase: ";
-    $db = new SQLiteDatabase('filename');
+	include ($_SERVER['DOCUMENT_ROOT'].'/scripts/connect.php');
+	include ($_SERVER['DOCUMENT_ROOT'].'/scripts/functions.php');
 	
 	if(!checkSession()){
 		echo '3:You need to be logged in the create a cause:Create';
@@ -22,16 +20,17 @@
 	$date = date("d-m-Y");
 	$slug = rand(10000,99999);
 
-    echo "blah";
-
 	$keyword = mysql_real_escape_string($keyword);
-    #$sql = "SELECT DISTINCT mp.mp_id,mp.first_name,mp.last_name FROM mp WHERE mp.mp_id IN (SELECT DISTINCT distance.mp_id from distance WHERE distance.dream_id IN (SELECT dream_id from policy WHERE UPPER(policy.title) LIKE UPPER('%".$keyword."%')) ORDER BY distance.distance LIMIT 10)";
-    $sql = "SELECT DISTINCT mp.mp_id,mp.first_name,mp.last_name FROM mp WHERE mp.mp_id IN (SELECT DISTINCT distance.mp_id from distance WHERE distance.dream_id IN (SELECT dream_id from policy WHERE UPPER(policy.title) LIKE UPPER('%".$keyword."%')) ORDER BY distance.distance LIMIT 10)";
+    #$sql = "SELECT DISTINCT mp.mp_id,mp.first_name,mp.last_name FROM mp WHERE mp.mp_id IN (SELECT DISTINCT distance.mp_id from distance WHERE distance.dream_id IN (SELECT dream_id from policy WHERE UPPER(policy.title) LIKE UPPER('%".$keyword."%')) ORDER BY distance.distance)";
+    $sql = "SELECT DISTINCT mp.mp_id,mp.first_name,mp.last_name FROM mp LIMIT 5";
 	$results = mysql_query($sql);
-    if (!$result) {
+    if (!$results) {
         echo "Invalid query: ".mysql_error();
     }
-    echo $results;
-    echo "blah";
+    echo "<ul>";
+    while ($row = mysql_fetch_array($results)) {
+        echo "<li>".$row['first_name']." ".$row['last_name']."</li>\n";
+    }
+    echo "</ul>";
 	exit;
 ?>
