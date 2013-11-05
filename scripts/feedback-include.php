0f936ffecf52b346a0ceb1c493b83ed77c8808a7
sanjaypoyzer <sanjaypoyzer@gmail.com>
2013-11-05 22:12:59 +0000

    (should have been 2 seperate commits) 1. moved the feedback div over to the left. 2. possibly fixed gitignore untracked files by clearing cache

--- scripts/feedback-include.php
+++ scripts/feedback-include.php
@@ -1,44 +1,4 @@
 <!-- Feedback Button -->
-<style type="text/css">
-div.feedback{
-	position: fixed;
-	top: calc(50% - 200px);
-	right: calc(-250px + 1.6em);
-	background: rgba(255, 255, 255, 0.8);
-	border: solid 2px black;
-	border-radius: 15px;
-	width: 250px;
-	padding: 1em;
-	z-index: 999;
-	-webkit-transition: 1s;
-	-moz-transition: 1s;
-	-o-transition: 1s;
-	-ms-transition: 1s;
-	transition: 1s;
-}
-div.feedback:hover{
-	right:-50px;
-}
-div.feedback form{
-	position: relative;
-	width: 100%;
-	left: 2em;
-}
-div.feedback h2{
-	-webkit-transform:rotate(270deg);
-	-moz-transform:rotate(270deg);
-	-o-transform:rotate(270deg);
-	-ms-transform:rotate(270deg);
-	transform:rotate(270deg);
-	position: absolute;
-	left: -3.7em;
-	top: 4em;
-}
-div.feedback form input, div.feedback form textarea{
-	display: block;
-	margin-bottom: 1em;
-}
-</style>
 <div class="feedback">
 	<form id='feedbackform' method='post' action='#' onsubmit="return false;">
 		<h2>Feedback?</h2>
@@ -46,7 +6,7 @@ div.feedback form input, div.feedback form textarea{
 		Email: <input type="text" id="feedbackemail" name="feedbackemail" />
 		Message: <textarea rows="4" id="feedbackmsg" id="feedbackmsg"></textarea>
 		<input type="submit" id="feedbackbtn" value="Submit Feedback" onclick='submitFeedback(); return false;' />
-		<?php echo CAUSEHUB_VERSION; ?>
+		<span>Version: <?php echo CAUSEHUB_VERSION; ?></span>
 	</form>
 </div>
 <script src="/scripts/feedback.js"></script>
