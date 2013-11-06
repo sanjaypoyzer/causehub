39eaf4d6e6b129d654eda83bbf042b6a1262dbf4
Jamie Davies <viralpickaxe@gmail.com>
2013-11-06 21:18:03 +0000

    Removed html for social buttons will do another day

--- cause_dynamic/index.php
+++ cause_dynamic/index.php
@@ -109,7 +109,6 @@
 			<!-- DONE DESCRIPTION RENDERING -->
 		</section>
 		<section class="knowledgeBaseSummary">
-			<a href='#'>Share to Facebook</a> <a href='#'>Share to twitter</a>
 			<h2>Knowledge Base</h2>
 			<ul>
 			<?php
