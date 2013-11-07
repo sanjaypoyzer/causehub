0fea9cb66f841c8a47ad3f0546610abddbc581de
Jamie Davies <viralpickaxe@gmail.com>
2013-11-07 10:10:00 +0000

    Setting to proper version

--- scripts/connect.php
+++ scripts/connect.php
@@ -5,16 +5,16 @@
 	define(CAUSEHUB_VERSION_NAME, 'Alpha');
 	define(CAUSEHUB_VERSION_MAJOR, '1');
 	define(CAUSEHUB_VERSION_MINOR, '0');
-	define(CAUSEHUB_VERSION_VERSION, '0');
-	define(CAUSEHUB_VERSION_BUILD, '0');
+	define(CAUSEHUB_VERSION_VERSION, '7');
+	define(CAUSEHUB_VERSION_BUILD, '5b');
 	define(CAUSEHUB_VERSION, CAUSEHUB_VERSION_NAME.' '.CAUSEHUB_VERSION_MAJOR.'.'.CAUSEHUB_VERSION_MINOR.'.'.CAUSEHUB_VERSION_VERSION.'.'.CAUSEHUB_VERSION_BUILD.'');
 
 ///CAUSEHUB INFO END
 
 
 $db_host = "localhost";
-$db_username = "root";
-$db_pass = "root";
+$db_username = "causehub";
+$db_pass = "pass";
 $db_name = "causehub";
 
 mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to MySQL");
