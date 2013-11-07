2663649150e6b024085820b8edb9e38d7e5f60c6
Jamie Davies <viralpickaxe@gmail.com>
2013-11-07 13:46:26 +0000

    Cut down on feedback.js

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,69 +1,70 @@
 function submitFeedback(){
-        var error = false;
-        document.getElementById('feedbackname').style.borderColor = '#999';
-        document.getElementById('feedbackemail').style.borderColor = '#999';
-        document.getElementById('feedbackmsg').style.borderColor = '#999';
-        if(document.getElementById('feedbackname').value==''){
+        var feedbackname = document.getElementById('feedbackname');
+        var feedbackemail = document.getElementById('feedbackemail');
+        var feedbackmsg = document.getElementById('feedbackmsg');
+        var feedbackbtn = document.getElementById('feedbackbtn');
+
+        feedbackname.style.borderColor = '#999';
+        feedbackemail.style.borderColor = '#999';
+        feedbackmsg.style.borderColor = '#999';
+        if(feedbackname.value==''){
             alertify.log('Please enter your name to submit feedback', 'error');
-            document.getElementById('feedbackname').style.borderColor = 'red';
-            error = true;
+            feedbackname.style.borderColor = 'red';
             return false;
         }
-        if(document.getElementById('feedbackemail').value==''){
+        if(feedbackemail.value==''){
             alertify.log('Please enter your email to submit feedback', 'error');
-            document.getElementById('feedbackemail').style.borderColor = 'red';
-            error = true;
+            feedbackemail.style.borderColor = 'red';
             return false;
         }
-        if(document.getElementById('feedbackmsg').value==''){
+        if(feedbackmsg.value==''){
             alertify.log('Please enter a message to submit feedback', 'error');
-            document.getElementById('feedbackmsg').style.borderColor = 'red';
-            error = true;
+            feedbackmsg.style.borderColor = 'red';
             return false;
         }
     
-    var data = 'name=' + document.getElementById('feedbackname').value + '&email=' + document.getElementById('feedbackemail').value + '&msg=' + document.getElementById('feedbackmsg').value;
+    var data = 'name=' + feedbackname.value + '&email=' + feedbackemail.value + '&msg=' + feedbackmsg.value;
         $.ajax({
         type  : 'POST',
          url  : '/scripts/processing/feedback.php',
          data : data,
          beforeSend : function() {
-             document.getElementById('feedbackbtn').disabled = true;
-             document.getElementById('feedbackname').disabled = true;
-             document.getElementById('feedbackemail').disabled = true;
-             document.getElementById('feedbackmsg').disabled = true;
-             document.getElementById('feedbackbtn').value = 'Submitting';
+             feedbackbtn.disabled = true;
+             feedbackname.disabled = true;
+             feedbackemail.disabled = true;
+             feedbackmsg.disabled = true;
+             feedbackbtn.value = 'Submitting';
          },
          error : function() {
-             document.getElementById('feedbackbtn').disabled = false;
-             document.getElementById('feedbackname').disabled = false;
-             document.getElementById('feedbackemail').disabled = false;
-             document.getElementById('feedbackmsg').disabled = false;
-             document.getElementById('feedbackbtn').value = 'Submit Feedback';
+             feedbackbtn.disabled = false;
+             feedbackname.disabled = false;
+             feedbackemail.disabled = false;
+             feedbackmsg.disabled = false;
+             feedbackbtn.value = 'Submit Feedback';
              alertify.log('Unable to submit feedback due to an unexpected error, please try again later', 'error');
              return false;
          },
          success : function (response) {
              var array = response.split(':');
              if(array[0]=='1'){
-                document.getElementById('feedbackbtn').value = 'Submitted';
-                document.getElementById('feedbackname').value = '';
-                document.getElementById('feedbackemail').value = '';
-                document.getElementById('feedbackmsg').value = '';
-                document.getElementById('feedbackname').disabled = false;
-                document.getElementById('feedbackemail').disabled = false;
-                document.getElementById('feedbackmsg').disabled = false;
-                document.getElementById('feedbackbtn').disabled = false;
+                feedbackbtn.value = 'Submitted';
+                feedbackname.value = '';
+                feedbackemail.value = '';
+                feedbackmsg.value = '';
+                feedbackname.disabled = false;
+                feedbackemail.disabled = false;
+                feedbackmsg.disabled = false;
+                feedbackbtn.disabled = false;
                 alertify.log('Thanks for your feedback!', 'success');
                 setTimeout(function (){
-                    document.getElementById('feedbackbtn').value = 'Submit Feedback';
+                    feedbackbtn.value = 'Submit Feedback';
                 }, 2000);
              } else {
-                document.getElementById('feedbackname').disabled = false;
-                document.getElementById('feedbackemail').disabled = false;
-                document.getElementById('feedbackmsg').disabled = false;
-                document.getElementById('feedbackbtn').disabled = false;
-                document.getElementById('feedbackbtn').value = 'Submit Feedback';
+                feedbackname.disabled = false;
+                feedbackemail.disabled = false;
+                feedbackmsg.disabled = false;
+                feedbackbtn.disabled = false;
+                feedbackbtn.value = 'Submit Feedback';
                 alertify.log(array[1], 'error');
              }
              return false;
