81654027879fcc76d745436b0e64a5186879b3de
Jamie Davies <viralpickaxe@gmail.com>
2013-11-07 10:30:59 +0000

    Added favicon.ico

--- scripts/extra.js
+++ scripts/extra.js
@@ -16,3 +16,15 @@ $(document).ajaxStop(function() {
 	NProgress.done();
 	console.log('Nprogress > Ajax Stop');
 });
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
+changeFavicon('http://www.iconj.com/ico/4/q/4qchpnn7ci.ico');
