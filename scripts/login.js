405de32d79ad45c6709add4e94fc5c470fad668f
Jamie Davies <viralpickaxe@gmail.com>
2013-11-07 14:52:37 +0000

    Cut down on login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,61 +1,63 @@
 function login(){
-    	var error = false;
-    	document.getElementById('u').style.borderColor = '#999';
-    	document.getElementById('p').style.borderColor = '#999';
-    	if(document.getElementById('u').value==''){
+    	var u = document.getElementById('u');
+        var p = document.getElementById('p');
+        var signinbtn = document.getElementById('signinbtn');
+
+
+    	u.style.borderColor = '#999';
+    	p.style.borderColor = '#999';
+    	if(u.value==''){
     		alertify.log('No Username Entered', 'error');
-    		document.getElementById('u').style.borderColor = 'red';
-    		error = true;
+    		u.style.borderColor = 'red';
     		return false;
     	}
-    	if(document.getElementById('p').value==''){
+    	if(p.value==''){
     		alertify.log('No Password Entered', 'error');
-    		document.getElementById('p').style.borderColor = 'red';
-    		error = true;
+    		p.style.borderColor = 'red';
     		return false;
     	}
 	
-	var data = 'u=' + document.getElementById('u').value + '&p=' + document.getElementById('p').value;
+	var data = 'u=' + u.value + '&p=' + p.value;
     	$.ajax({
         type  : 'POST',
          url  : '/scripts/processing/login.php',
          data : data,
          beforeSend : function() {
-             document.getElementById('signinbtn').disabled = true;
-             document.getElementById('u').disabled = true;
-             document.getElementById('p').disabled = true;
-             document.getElementById('signinbtn').value = 'Logging in';
+             signinbtn.disabled = true;
+             u.disabled = true;
+             p.disabled = true;
+             signinbtn.value = 'Logging in';
          },
          error : function() {
-             document.getElementById('signinbtn').disabled = false;
-             document.getElementById('u').disabled = false;
-             document.getElementById('p').disabled = false;
-             document.getElementById('signinbtn').value = 'Error';
-             document.getElementById('p').value = '';
+             signinbtn.disabled = false;
+             u.disabled = false;
+             p.disabled = false;
+             signinbtn.value = 'Error';
+             p.value = '';
              alertify.log('Error connecting to DB', 'error');
              return false;
          },
          success : function (response) {
              var array = response.split(':');
              if(array[0]=='1'){
-             	document.getElementById('signinbtn').value = array[2];
+             	signinbtn.value = array[2];
    		        window.location.href = '/';
              } else if(array[0]=='2'){
-                document.getElementById('u').disabled = false;
-                document.getElementById('p').disabled = false;
-                document.getElementById('signinbtn').disabled = false;
-                document.getElementById('signinbtn').value = array[2];
+                u.disabled = false;
+                p.disabled = false;
+                signinbtn.disabled = false;
+                signinbtn.value = array[2];
                 alertify.log(array[1], 'error');
-           		document.getElementById('u').style.borderColor = 'red';
-           		document.getElementById('p').style.borderColor = 'red';
-           		document.getElementById('p').value = '';
+           		u.style.borderColor = 'red';
+           		p.style.borderColor = 'red';
+           		p.value = '';
              } else {
-                document.getElementById('u').disabled = false;
-                document.getElementById('p').disabled = false;
-                document.getElementById('signinbtn').disabled = false;
-                document.getElementById('signinbtn').value = array[2];
+                u.disabled = false;
+                p.disabled = false;
+                signinbtn.disabled = false;
+                signinbtn.value = array[2];
            		alertify.log(array[1], 'error');
-           		document.getElementById('p').value = '';
+           		p.value = '';
              }
              return false;
          }
