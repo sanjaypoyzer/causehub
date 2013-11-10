0ab1744dded2f4380b71e79d948cb820d9d04f0e
Jamie Davies <viralpickaxe@gmail.com>
2013-11-10 14:14:20 +0000

    Connection info for new database

--- scripts/connect.php
+++ scripts/connect.php
@@ -12,15 +12,10 @@
 ///CAUSEHUB INFO END
 
 
-$db_host = "localhost";
-<<<<<<< HEAD
-$db_username = "causehub";
-$db_pass = "pass";
-=======
-$db_username = "root";
-$db_pass = "root";
->>>>>>> 0f6913c4ad1a7e1d92faeae0772346ecebe91666
-$db_name = "causehub";
+$db_host = "93.189.2.202";
+$db_username = "causehub_user";
+$db_pass = "jamieislame69";
+$db_name = "causehub_causehub";
 
 mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to MySQL");
 mysql_select_db("$db_name") or die ("No Database");
