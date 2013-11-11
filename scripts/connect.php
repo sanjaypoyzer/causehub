98ea50d5e7c2ab261d5b587a45056bea467663ae
Jamie Davies <viralpickaxe@gmail.com>
2013-11-11 18:10:08 +0000

    Create connect.php

---
+++ scripts/connect.php
@@ -0,0 +1,9 @@
+<?php
+$db_host = "93.189.2.202";
+$db_username = "causehub_user";
+$db_pass = "jamieislame69";
+$db_name = "causehub_causehub";
+
+mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to MySQL");
+mysql_select_db("$db_name") or die ("No Database");
+?>
