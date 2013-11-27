b2145b92bcd5ee6b9347ad577563900fb41f9a4f
Jamie Davies <viralpickaxe@gmail.com>
2013-11-27 14:56:49 +0000

    Refining wordcount.php more

--- wordcount.php
+++ wordcount.php
@@ -2,7 +2,7 @@
 	session_start();
 	include ($_SERVER['DOCUMENT_ROOT'].'/scripts/connect.php');
 
-	$sql = mysql_query("SELECT * FROM causes WHERE id='24' AND deleted='0'");
+	$sql = mysql_query("SELECT * FROM causes WHERE deleted='0'");
 	while($array = mysql_fetch_array($sql)){
 
 		$json = $array['description'];
@@ -24,6 +24,8 @@
 				
 				$words[$arr[$y]]['word'] = $arr[$y];
 				$words[$arr[$y]]['num'] += 1;
+				$words[$arr[$y]]['locations'][count($words[$arr[$y]]['locations'])]['cid'] = $array['id'];
+				$words[$arr[$y]]['locations'][count($words[$arr[$y]]['locations'])-1]['type'] = $jsondescarray['data'][$i]['type'];
 				$total += 1;
 
 			}
@@ -52,7 +54,7 @@
 	?>
 	</div>
 	<div id='word' style='font-size: 45px; margin-top: 50px; text-align: center; width: 100%; font-weight: bold;'>Hover over a bar to show the word</div>
-	<br><br><br>
+	<br><br><br><br>
 	<?php
 		for($i=0;$i<count($words);$i++){
 			if($words[$i]['num']>2 && strlen($words[$i]['word'])>=3){
@@ -61,9 +63,22 @@
 				} else {
 					$extra = '';
 				}
-				echo '<span style="font-size: '.$words[$i]['percent'].'em; '.$extra.'">'.$words[$i]['word'].'</span> ';
+
+				$locations = '';
+				for($x=0;$x<count($words[$i]['locations']);$x++){
+					$locations .= $words[$i]['locations'][$x]['cid'].' ('.$words[$i]['locations'][$x]['type'].')';
+					if($x!=(count($words[$i]['locations'])-1)){
+						$locations .= ' => ';
+					}
+				}
+
+				$movercommand = "document.getElementById('locations').innerHTML = '".$locations."'; this.style.fontWeight = 'bold';";
+				$moutcommand = "document.getElementById('locations').innerHTML = ''; this.style.fontWeight = 'normal';";
+				echo '<span style="font-size: '.$words[$i]['percent'].'em; '.$extra.' cursor: pointer;" onmouseover="'.$movercommand.'" onmouseout="'.$moutcommand.'">'.$words[$i]['word'].'</span> ';
 			}
 		}
 	?>
+	<br><br>
+	<div id='locations'></div>
 </body>
 </html>
